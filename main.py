c1e3dc92c53ab05d6bc1b8b4d76a896054f38fea
simple029 <44561635+simple029@users.noreply.github.com>
2023-10-13 15:35:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
         data = {
             "touser": theuser,
             "template_id": config.template_id,
-            "url": "http://weixin.qq.com/download",
+            "url": "http://weixin.qq.com",
             "topcolor": "#FF0000",
             "data": {
                 "date": {
